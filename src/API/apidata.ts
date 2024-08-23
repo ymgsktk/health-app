4b1a7d5e48dd87458def096679da5f70ebc6f3a9
ymgsktk <8ymgsktk11@gail.com>
2024-08-23 19:28:26 +0900

    [WIP]Add mockAPI data. Reorganize API data file.

---
+++ src/API/apidata.ts
@@ -0,0 +1,251 @@
+export const USER_INFO = {
+    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMyIsImp0aSI6IjM1ZmI5OTgyM2YzNWJhMDU4ZDM2MDYxNmMxMmQ0NTViN2U1NDhjZWFlOGJlMjJkYTRhMjNlMDRiMTdhZTJkYTcyZGI2YThlYTVmYmFlNTUzIiwiaWF0IjoxNzIzNjI4OTQxLCJuYmYiOjE3MjM2Mjg5NDEsImV4cCI6MTc1NTE2NDk0MSwic3ViIjoiMTgzNzYxIiwic2NvcGVzIjpbXX0.OD0-b8Uz8uFpbadgFpR-Edac_l7IeJ41JH8wf8H8q5tjkJJrWEjnEIR7hNUMtjmgszYNzlLDtIFBb1pHsHDn6PPrtFYg4ioioAqmPlVjIV8WYUV7TjSLaxcSohO4rudbTMbmT7-gDz61Xtz9_HdCWOmMOuBq65FEux-e3PS6-XLDl0hAAjBp-11G8jUoEdnyrCXD6hux56bR_wFX0KeaDsAtUE_oXIA2AN1pJ4ZNO9aJmMdi7_xT3hA4RNrSmUnmBY7-U-tESZG6A19ZftYVxteEZDHdgaPe3wTyUYZlADpcrzMt6KfMJ_WjZvgshElZTYh2vyxTVlmpcNmg__QRuQwlfG96Q_2D7NF8nppiwm7uQT3ma6UDnD0_EW2skA8qWEaIyWCwNlRsNyHGbqeB5nW-XD9jrIUyf7fHkzBEw_-qFFXQmFuEy5VKalDs-sZZEmn53UdoUTf9MpITtgtUkkftVCi0vK3J3i0oLvS_7lTFbvfNdL0GanQhj_xoOeRYH_CO1UOzP_GhpdyqTCoOZIHhRTCDiNXzndgQmj7hI_uHBHsuDLE3P58-0rgUJp_QaNUmIpwYu2ycTWBsCjbWarYh0XfP_XFPzjRt1K6AgWzaeJYTdqQd4xErY3ELSShRiy-lXO4rV3gljEg6ZZ8jwU9VPPnGg17f1J7t5tNoYtk",
+    email: "yamagishi@rikkeisoft.com",
+    password: "12345678"
+  };
+
+export const posts = [
+    {
+      id: 1,
+      title: "Yamagishi Katsuki",
+      teamdescription: "Ant Design, a design language for backgraound applications, is refined by AntUED Team.",
+      description: "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+      imageUrl: "https://www.kenkomayo.com/img/goods/L/recipe12920-002_85506bdb685f4f009a5c11c2f93a7396.jpg",
+      iconUrl: "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png",
+      star: 156,
+      likes: 156,
+      comments: 2,
+      date: "2024/8/21",
+    },
+    {
+      id: 2,
+      title: "ant design part 10",
+      teamdescription: "Ant Design, a design language for backgraound applications, is refined by AntUED Team.",
+      description: "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+      imageUrl: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/03/a0003440/img/basic/a0003440_main.jpg",
+      iconUrl: "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_2.png",
+      star: 156,
+      likes: 156,
+      comments: 2,
+      date: "2024/8/20",
+    },
+    {
+      id: 3,
+      title: "ant design part 11",
+      teamdescription: "Ant Design, a design language for backgraound applications, is refined by AntUED Team.",
+      description: "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+      imageUrl: "https://static.vinwonders.com/production/com-ga-nha-trang-banner-1.jpg",
+      iconUrl: "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_3.png",
+      star: 156,
+      likes: 156,
+      comments: 2,
+      date: "2024/8/19",
+    }
+  ];
+
+export const menus = [
+    {
+      id: 1,
+      title: "hamburger steak",
+      description: "juicy steak",
+      imageUrl: "https://vietnam-sketch.com/wp-content/uploads/2023/02/thumbnail-90948.png",
+      calorie: 350,
+      protein: 26,
+    },
+    {
+      id: 2,
+      title: "Salad",
+      description: "fresh salad",
+      imageUrl: "https://www.m-portugal.jp/global-image/units/img/6880-1-20160203153512_b56b19fa0389ee.jpg",
+      calorie: 50,
+      protein: 16,
+    },
+    {
+      id: 3,
+      title: "Chicken Breast",
+      description: "high protein",
+      imageUrl: "https://vietmaru.com/wp-content/uploads/2020/01/DSC00793.jpg",
+      calorie: 200,
+      protein: 31,
+    },
+    {
+      id: 4,
+      title: "Pasta",
+      description: "yummy pasta",
+      imageUrl: "https://s3-ap-northeast-1.amazonaws.com/concerto-prod/images/209/thumb_480.jpg?1615084506",
+      calorie: 150,
+      protein: 8,
+    },
+    {
+      id: 5,
+      title: "Spring roll",
+      description: "healthy cuisine",
+      imageUrl: "https://www.meiji.co.jp/meiji-shokuiku/worldculture/images/recipe/pic_vietnam2.jpg",
+      calorie: 50,
+      protein: 16,
+    },
+  ]
+
+export const material =[
+    {
+        id: 1,
+        title: "BEEF BONELESS",
+        calorie: 150,
+        protein: 18.5,
+        fat: 7.9,
+        type: "meat"
+    },
+    {
+        id: 2,
+        title: "BEEF SAUSAGES",
+        calorie: 313,
+        protein: 11.7,
+        fat: 28.4,
+        type: "meat"
+    },
+    {
+        id: 3,
+        title: "PORK",
+        calorie: 220,
+        protein: 13.4,
+        fat: 18.0,
+        type: "meat"
+    },
+    {
+        id: 4,
+        title: "BACON - HAM OF PIGS",
+        calorie: 362,
+        protein: 13.1,
+        fat: 34.1,
+        type: "meat"
+    },
+    {
+        id: 5,
+        title: "CHICKEN MEAT",
+        calorie: 122,
+        protein: 12.3,
+        fat: 7.7,
+        type: "meat"
+    },
+    {
+        id: 6,
+        title: "TURKEY MEAT",
+        calorie: 126,
+        protein: 16.1,
+        fat: 6.3,
+        type: "meat"
+    },
+    {
+        id: 7,
+        title: "MEAT OF CAMELS",
+        calorie: 174,
+        protein: 12.7,
+        fat: 13.2,
+        type: "meat"
+    },
+    {
+        id: 8,
+        title: "HEN EGGS",
+        calorie: 139,
+        protein: 10.7,
+        fat: 9.8,
+        type: "egg"
+    },
+    {
+        id: 9,
+        title: "FRESHWATER DIADROMOUS FISH FRESH",
+        calorie: 69,
+        protein: 10.9,
+        fat: 2.5,
+        type: "fish"
+    },
+    {
+        id: 10,
+        title: "DEMERSAL FISH FRESH",
+        calorie: 42,
+        protein: 8.3,
+        fat: 0.8,
+        type: "fish"
+    },
+    {
+        id: 11,
+        title: "MARINE FISH NES FRESH",
+        calorie: 64,
+        protein: 10.3,
+        fat: 2.2,
+        type: "fish"
+    },
+    {
+        id: 12,
+        title: "COW MILK, WHOLE FRESH",
+        calorie: 61,
+        protein: 3.3,
+        fat: 3.3,
+        type: "milk and cheese"
+    },
+    {
+        id: 13,
+        title: "YOGHURT",
+        calorie: 61,
+        protein: 3.5,
+        fat: 3.3,
+        type: "milk and cheese"
+    },
+    {
+        id: 14,
+        title: "PROCESSED CHEESE",
+        calorie: 103,
+        protein: 12.5,
+        fat: 4.5,
+        type: "milk and cheese"
+    },
+    {
+        id: 15,
+        title: "GOAT MILK",
+        calorie: 69,
+        protein: 3.6,
+        fat: 4.1,
+        type: "milk and cheese"
+    },
+    {
+        id: 16,
+        title: "CABBAGES",
+        calorie: 19,
+        protein: 1.0,
+        fat: 0.1,
+        type: "vegetable"
+    },
+    {
+        id: 17,
+        title: "ASPARAGUS",
+        calorie: 12,
+        protein: 1.6,
+        fat: 0.1,
+        type: "vegetable"
+    },
+    {
+        id: 18,
+        title: "TOMATOES",
+        calorie: 17,
+        protein:0.8,
+        fat: 0.2,
+        type: "vegetable"
+    },
+    {
+        id: 19,
+        title: "CAULIFLOWER",
+        calorie: 9,
+        protein:0.8,
+        fat: 0.1,
+        type: "vegetable"
+    },
+    {
+        id: 20,
+        title: "EGGPLANTS",
+        calorie: 21,
+        protein:0.9,
+        fat: 0.1,
+        type: "vegetable"
+    },
+
+]
